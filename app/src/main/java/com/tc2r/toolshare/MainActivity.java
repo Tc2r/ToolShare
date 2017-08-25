6fcecd69aab753a0e2eee4a9b3a843c160c9bc0d
Tc2r <Nudenniew@gmail.com>
2017-08-24 22:11:38 -0500

    Shows how to use retrofit to get data from our sql server.

--- app/src/main/java/com/tc2r/toolshare/MainActivity.java
+++ app/src/main/java/com/tc2r/toolshare/MainActivity.java
@@ -4,8 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
 	private ArrayList<ListingModel> listings = null;
 	private ListingModel tempModel;
+	private ProgressBar progressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 		// Initialize Variables
 		listings = new ArrayList<>();
 		recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+		progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
 
 		// TRY OUT EACH ONE OF THESE TO SEE WHAT THE LAYOUT MANAGER DOES.
@@ -34,30 +46,44 @@ public class MainActivity extends AppCompatActivity {
 		//	layoutManager = new GridLayoutManager(this, 2, LinearLayoutManager.HORIZONTAL, false);
 		//	layoutManager = new GridLayoutManager(this, 2, LinearLayoutManager.HORIZONTAL, true);
 
+		// Get Real Data From SQL Database.
 
-		// Create Fake models to use temporarily.
-		tempModel = new ListingModel(1, "Ttsukasa", "Can Opener For Sale", "I have a can opener if anyone needs it must be returned before nightfall", "Lombard, IL", "662-123-4567");
-		listings.add(tempModel);
-		tempModel = new ListingModel(2, "Rai", "Bike Mount for car", "Specifically fits tanks, to borrow it you have to wrestle it from my cold dead hands. Come at me!", "Collegeville, IL", "773-123-4567");
-		listings.add(tempModel);
-		tempModel = new ListingModel(3, "Ttsukasa", "Can Opener For Sale", "I have a can opener if anyone needs it must be returned before nightfall", "Lombard, IL", "662-123-4567");
-		listings.add(tempModel);
-		tempModel = new ListingModel(4, "Rai", "Bike Mount for car", "Specifically fits tanks, to borrow it you have to wrestle it from my cold dead hands. Come at me!", "Collegeville, IL", "773-123-4567");
-		listings.add(tempModel);
-		tempModel = new ListingModel(5, "Ttsukasa", "Can Opener For Sale", "I have a can opener if anyone needs it must be returned before nightfall", "Lombard, IL", "662-123-4567");
-		listings.add(tempModel);
-		tempModel = new ListingModel(6, "Rai", "Bike Mount for car", "Specifically fits tanks, to borrow it you have to wrestle it from my cold dead hands. Come at me!", "Collegeville, IL", "773-123-4567");
-		listings.add(tempModel);
-		tempModel = new ListingModel(7, "Ttsukasa", "Can Opener For Sale", "I have a can opener if anyone needs it must be returned before nightfall", "Lombard, IL", "662-123-4567");
-		listings.add(tempModel);
-		tempModel = new ListingModel(8, "Rai", "Bike Mount for car", "Specifically fits tanks, to borrow it you have to wrestle it from my cold dead hands. Come at me!", "Collegeville, IL", "773-123-4567");
-		listings.add(tempModel);
+		// Create a call for the data (request)
+		Call<List<ListingModel>> call = RetrofitInterface
+						.Factory
+						.getInstance()
+						.getEverything();
+
+
+		// Queue our request and listen for a response, on successful response, update recyclerview/adater
+		// on fail alert user.
+		call.enqueue(new Callback<List<ListingModel>>() {
+			@Override
+			public void onResponse(Call<List<ListingModel>> call, Response<List<ListingModel>> response) {
+				// Dismiss Progress Bar
+				progressBar.setVisibility(View.GONE);
+
+				// If response isn't an error
+				if (response.isSuccessful()) {
+					listings = (ArrayList<ListingModel>) response.body();
+					adapter = new ArushiAdapter(listings, getApplicationContext());
+					adapter.notifyDataSetChanged();
+					Log.d("JSON DATA: ", new GsonBuilder().setPrettyPrinting().create().toJson(response.body()));
+					recyclerView.setAdapter(adapter);
+				}
+			}
+
+			@Override
+			public void onFailure(Call<List<ListingModel>> call, Throwable t) {
+				// Dismiss Progress Bar
+				progressBar.setVisibility(View.GONE);
+				Log.e(TAG, "Unable To Connect To Server " + t.getMessage());
+			}
+		});
 
 		// Set up the recyclerview.
-		adapter = new ArushiAdapter(listings, this);
 		recyclerView.setHasFixedSize(true);
 		recyclerView.setLayoutManager(layoutManager);
-		recyclerView.setAdapter(adapter);
 	}
 
 }
